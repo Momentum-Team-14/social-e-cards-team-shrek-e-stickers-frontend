087ae065560c8118e7eb3056e876b95b8738e958
Nathan Zeager <nathanzeager@hotmail.com>
2022-09-21 16:29:50 -0400

    Added login page and buttons to move in and out of it

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
+import { Login } from './components/Login'
+
 // header will be built here, in return ()
 // if !isLoggedIn, return LoginPage. if isLoggedIn, return Homepage
 // **what vairables and models do we need to pass in?
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(true)
+
+  if (!isLoggedIn) {
+    return(<Login setIsLoggedIn={setIsLoggedIn} />)
+  }
+
   return (
     <>
     
       <div className='nathan'>Stickr App is running!!</div>
 
     <div>This matches</div>
+
+    <button onClick={() => setIsLoggedIn(false)}>Click to see Login Page</button>
     </>
   )
 }
