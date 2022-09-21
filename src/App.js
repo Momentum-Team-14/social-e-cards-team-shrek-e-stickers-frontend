6b91bc336e2e49a53f67d3a9b33902f94fc8fc55
aziza <azizadlittle@gmail.com>
2022-09-21 13:06:17 -0400

    Added components folder

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+// header will be built here, in return ()
+// if !isLoggedIn, return LoginPage. if isLoggedIn, return Homepage
+// **what vairables and models do we need to pass in?
 
 function App() {
   return (
     <>
+    
       <div className='nathan'>Stickr App is running!!</div>
+
     <div>This matches</div>
     </>
   )
 }
-
+// header built here **only displayed if logged in
 export default App;
