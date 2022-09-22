f2d2fdfbb8332d3b8b7039d9963f5c34db38777e
aziza <azizadlittle@gmail.com>
2022-09-22 17:22:33 -0400

    added component and resources folders

---
+++ src/components/Header.js
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+
+export const Header = () => {
+const [display, setDisplay] = useState ('none')
+const handleClick = () => {
+    if (display === 'none') {
+        setDisplay('block')
+    } else {
+        setDisplay('none')
+    }
+}
+const handleProfile = ()=> {
+    // how do I navigate to the user's profile? what useState should I use/create?
+}
+const handleSignOut = () => {
+    alert
+    // need to figure out how to make a confirm alert and make the buttons go baack to either the home page or login page
+}
+    return (
+        <div className="header">
+            <div className="logo">
+                <image src="--INSERT LOGO CUTOUT HERE--"/>
+            </div>
+            <div className="login-info" onClick={()=> handleClick}>
+                {username}
+            </div>
+            <div className="drop-down-menu" style={{display:display}}>
+                <div onClick={()=>handleProfile}>My Profile</div>
+                <div onClick={()=>handleSignOut}>Sign Out</div>
+            </div>
+        </div>
+    )
+}
