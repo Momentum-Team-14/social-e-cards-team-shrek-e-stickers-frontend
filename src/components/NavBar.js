4724f4612d434ae9f462dae09864738238b7c7c9
Nathan Zeager <nathanzeager@hotmail.com>
2022-09-27 12:44:02 -0400

    Added login functionality, updated some of the urls, added _redirects file for Netlify

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,7 @@
 export const NavBar = () => {
     return(
         <>
-            <div><a href='/'>Homepage</a></div>
+            <div><a href='/stickrs'>Homepage</a></div>
             <div><a href='/profile'>Profile</a></div>
             <div>Create Stickr</div>
         </>
